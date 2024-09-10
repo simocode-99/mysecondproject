e7d6d82f1003eca7ff2a9086963996b7b079e7f3
user.email <simoaraja17@gmail.com>
2024-09-10 20:29:21 +0100

    Adding some checkBox and styling them with CSS and make them dynamicly with JavaScript

--- test.js
+++ test.js
@@ -1,1 +1,20 @@
-document.alert('HELLO')
+const mygitbox    = document.getElementById('mygitbox');
+const mygithubbox = document.getElementById('mygithubbox');
+const myH2        = document.getElementById('myH2');
+const mybtn       = document.getElementById('mybtn');
+
+mybtn.addEventListener('click', event => {
+    event.preventDefault()
+    if(mygitbox.checked && mygithubbox.checked){
+        myH2.textContent = 'All Skills Completed';
+        myH2.style.display = 'block';
+    }
+    else if(!mygitbox.checked && !mygithubbox.checked) {
+        myH2.textContent = 'there is No Skill Completed !';
+        myH2.style.display = 'block';
+    }
+    else{
+        myH2.style.display = 'block';
+        myH2.textContent = 'Complete Skill !';
+    }
+} )
